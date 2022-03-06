2d41d386458a2f0a732b89b9f676ff48aea00bb4
ConteMan <real.conteman@gmail.com>
2022-03-06 19:00:38 +0800

    feat: init with Koa && Next.js;

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
 import Koa from 'koa'
+import next from 'next'
+import Router from '@koa/router'
+import cors from '@koa/cors'
 
-const app = new Koa()
-const msg = 'Hello World, By ConteMan~'
+const port = parseInt(process.env.PORT, 10) || 80
+const dev = process.env.NODE_ENV !== 'production'
 
-app.use(async(ctx: Koa.Context): Promise<void> => {
-  ctx.body = msg
+const app = next({ dev })
+
+app.prepare().then(() => {
+  const server = new Koa()
+  const router = new Router()
+
+  router.get('/one', async(ctx) => {
+    const res = await fetch('https://api.isconte.com/ones/random', {
+      method: 'GET',
+      headers: {
+        'x-user-agent': 'conte-world-f'
+      }
+    }).then((res: any) => res.json())
+    ctx.body = res.data
+  })
+
+  router.all('(.*)', async(ctx: any) => {
+    ctx.redirect('/one')
+    ctx.status = 301
+  })
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200
+    await next()
+  })
+
+  server.use(cors())
+  server.use(router.routes())
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV}`)
+  })
 })
-
-app.listen(7777)
